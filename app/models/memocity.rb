e910b2829631372dadd421f567a496555feb2bd0
Nico Fuenzalida <nico01f@gmail.com>
2016-02-18 21:23:44 -0300

    date validation finished

--- app/models/memocity.rb
+++ app/models/memocity.rb
@@ -5,13 +5,16 @@ class Memocity < ActiveRecord::Base
   validates :phoneNumberMemo, presence: true, length: { maximum: 11}, format: { with: VALID_PHONE_NUMBER_REGEX }
   validates :contentMemo, presence: true, length: { maximum: 120 }
   validates :activationDateMemo, presence: true
-
+  validate :activation_date_greater_than_now
   private
 
   def capitalize_content
     self.contentMemo = contentMemo.capitalize
   end
 
-
-
+  def activation_date_greater_than_now
+    if self.activationDateMemo <= Time.now
+      errors.add(:base, "Fecha debe ser mayor a la actual")
+    end
+  end
 end
